11940984a7d34136089eefc586526443624a6754
Prathamesh-Killedar <prathameshkilledar05@gmail.com>
2025-10-29 20:56:35 +0530

    Day2 CSS practice and Basic JavaScript completed

---
+++ JSbasic/test.js
@@ -0,0 +1,25 @@
+console.log("Hello all")
+
+//alert("Hellooo")
+
+let message;
+message= "Hello";
+console.log(message);
+
+
+let a=10, b=5;
+result = a+b;
+console.log(result);
+
+
+let name="xyz", age=22, city="pune";
+console.log(`Hello I am ${name}, ${age} years old from ${city}`);
+
+console.log(typeof(name));
+
+
+function greet(name) {
+    console.log("Hello " + name)
+}
+
+greet("Prathamesh");
