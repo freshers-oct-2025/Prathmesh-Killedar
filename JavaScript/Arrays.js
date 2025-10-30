6067fe8ea1b1b0ab3644fa4b5306bc51f5755643
Prathamesh-Killedar <prathameshkilledar05@gmail.com>
2025-10-30 20:12:57 +0530

    Day3 JavaScript Basics Completed

---
+++ JavaScript/Arrays.js
@@ -0,0 +1,44 @@
+const arr = [1,2,3,4]
+console.log(arr);
+console.log(arr[1]);
+
+arr.push(5)
+console.log(arr);
+
+arr.unshift(8)
+console.log(arr);
+arr.shift()
+console.log(arr);
+
+console.log(arr.includes(4));
+
+const newArr = arr.join()
+console.log(arr);
+console.log(newArr);
+console.log(typeof(newArr));
+
+//Slice does not manipulate original array
+const arr1 = arr.slice(1,3)
+console.log(arr1);
+console.log(arr);
+
+//Splice manipulate original array
+const arr2 = arr.splice(1,3)
+console.log(arr2);
+console.log(arr);
+
+const mhCity = ["pune", "mumbai"]
+const oCity = ["delhi"]
+
+const city = mhCity.concat(oCity)
+const city1 = [...mhCity, ...oCity]
+console.log(city1);
+
+
+
+console.log("Hello", a);
+var a =null;
+console.log(a);
+a=5;
+console.log(a);
+
---
+++ JavaScript/Basic.js
@@ -0,0 +1,55 @@
+ // Octal Numbers
+let num = 0o77;
+console.log(num);
+
+// e = 10^5
+let value= 3.14e5;
+console.log(value);
+
+let num2 = 0.0000005;
+console.log(num2);
+
+let err="Error occured";
+let hasErr=Boolean(err);
+
+if(err){
+    console.log(err);
+}
+
+
+let isLogedIn = 1
+
+let boolenIsLoggedIn = Boolean(isLogedIn)
+console.log(boolenIsLoggedIn);
+
+
+let lang = "JavaScript"
+let langname = lang
+langname="Java"
+
+console.log(lang);
+console.log(langname);
+
+
+console.log(lang.length);
+console.log(lang.toUpperCase());
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min =15
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min +1)+min));
+
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+
+//months starts from 0 
+console.log(myDate.getMonth()+1);
+
